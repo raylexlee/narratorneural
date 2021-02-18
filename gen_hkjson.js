56bdbf3be3b9603917028cca2b56c2304a341260
raylexlee <raylexym57lee@gmail.com>
2021-02-18 16:47:47 +0800

    Use proper naming.

--- gen_hkjson.js
+++ gen_hkjson.js
@@ -1,5 +1,4 @@
 const fs=require('fs');
 const rawdata = fs.readFileSync('./voices.json', {encoding:'utf8', flag:'r'});
 const speakers = JSON.parse(rawdata).VoiceNames;
-const hkers = speakers.filter(s => /^(en|zh).*Neural$/.test(s.ShortName)).map(s => [s.ShortName, s.LocalName, s.Gender]);
-console.log(hkers);
+fs.writeFileSync('./speakers.json', JSON.stringify(speakers.filter(s => s.VoiceType === "Neural")));
